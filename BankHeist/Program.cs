7d963e0010e5b5b7c2ca27619ef43586ae0ada83
Jacob <wittenberg.jacob@gmail.com>
2020-03-24 21:21:29 -0500

    Phase one works

--- BankHeist/Program.cs
+++ BankHeist/Program.cs
@@ -7,6 +7,21 @@ namespace BankHeist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan your Heist!");
+            Console.ReadLine();
+
+            Console.WriteLine("Who are you recruiting to join your heist?");
+            var teamMember = new TeamMember(Console.ReadLine());
+
+
+            Console.WriteLine("How skilled is this person at committing heists? (please enter a number larger than zero)");
+            teamMember.SkillLevel = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("How Brave is this person? (please enter a decimal between 0.0 and 2.0)");
+            teamMember.CourageFactor = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write($"{teamMember.Name} is part of your crew now! They have a skill rating of {teamMember.SkillLevel} and have a courage score of {teamMember.CourageFactor}");
+            Console.ReadLine();
         }
     }
 }
--- BankHeist/TeamMember.cs
+++ BankHeist/TeamMember.cs
@@ -6,5 +6,41 @@ namespace BankHeist
 {
     class TeamMember
     {
+        public string Name { get; set; }
+        private int _skillLevel;
+        private double _courageFactor;
+
+        public int SkillLevel
+        {
+            get => _skillLevel;
+            set
+            {
+                if (value > 0)
+                {
+                    _skillLevel = value;
+                }
+            }
+        }
+
+        public double CourageFactor
+        {
+            get => _courageFactor;
+            set
+            {
+                if (value >= 0.0 &&  value <= 2.0)
+                {
+                    _courageFactor = value;
+                }
+                else
+                {
+                    _courageFactor = 1.0;
+                }
+            }
+        }
+
+        public TeamMember(string name)
+        {
+            Name = name;
+        }
     }
 }
